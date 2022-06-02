71b50d503a26d50c2e20e0a96b17f90b7804a76b
Gadilar <wenbofu16@gmail.com>
2022-06-03 00:25:00 +0800

    2022-06-03

---
+++ delete-node-in-a-bst/solute.go
@@ -0,0 +1,36 @@
+package solute
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == key {
+		return mergeTree(root.Left, root.Right)
+	}
+	if root.Val > key {
+		root.Left = deleteNode(root.Left, key)
+	} else {
+		root.Right = deleteNode(root.Right, key)
+	}
+	return root
+}
+
+func mergeTree(leftTree, rightTree *TreeNode) *TreeNode {
+	if leftTree == nil {
+		return rightTree
+	}
+
+	if rightTree == nil {
+		return leftTree
+	}
+
+	newTree := mergeTree(leftTree, rightTree.Left)
+	rightTree.Left = newTree
+	return rightTree
+}
